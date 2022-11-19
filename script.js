a139cbb62a8ec7898a174d60d6052dd4f65c1fbd
shivarajkumar <shivaraj1654@gmail.com>
2022-11-19 14:15:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', calculatetime)
+
+function calculatetime(){
+    var date = new Date();
+    var daynumber = date.getDay();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var ampm = hour >= 12 ?'PM' : 'AM';
+    var daynames = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+    hour = hour % 12;
+    // hour = hour ? 12 : '12';
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+    document.getElementById("day").innerHTML = daynames[daynumber];
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("ampm").innerHTML = ampm;
+
+    setTimeout(calculatetime, 200);
+
+}
